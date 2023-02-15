2483df2b71baa81c94104418dc0c648ceb2879d9
Xinxin Mei <xmei@jlab.org>
2023-02-15 15:55:30 -0500

    Add a script to merge the csv results into a single csv file

---
+++ scripts/data_processing/fc_merge_csv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Read the csv files at $INPUT_PATH and merge them into a big one $OUTPUT_FILE_PATH
+# Sample usage: sh scripts/data_processing/fc_merge_csv.sh res_2023/Feb_13 res_2023/fc_T4_f32.csv
+
+INPUT_PATH=$1
+OUTPUT_FILE_PATH=$2
+
+# the same as that in fc.py
+HEADER_LINE="device,input_type,layers,nodes,batch_size,input_size,output_size,#params,duration,tflops"
+
+#echo $HEADER_LINE > ${OUTPUT_FILE_PATH}
+
+for filename in ${INPUT_PATH}/*/*/benchmark_*.csv; do
+  echo "Reading input from " $filename
+  while IFS=, read -r line; do
+    if [[ $line == ${HEADER_LINE} ]]; then
+      continue
+    fi
+    echo $line >> ${OUTPUT_FILE_PATH}  # >> to write to the end
+  done <$filename
+done;
