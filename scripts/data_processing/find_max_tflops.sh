69744b36790d37465a58852f9cfd7b72b45f61dd
Xinxin Mei <xmei@jlab.org>
2023-04-18 00:55:20 -0400

    Fix heatmap labels

--- scripts/data_processing/find_max_tflops.sh
+++ scripts/data_processing/find_max_tflops.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# Read the csv files at $INPUT_PATH and merge them into a big one $OUTPUT_FILE_PATH
-# Sample usage: sh scripts/data_processing/fc_merge_csv.sh res_2023/Feb_13 res_2023/fc_T4_f32.csv
+## Read the csv files located at $INPUT_PATH, find the highest tflops by data type.
+## Output the rows with highest tflops under current folder.
 
 INPUT_PATH=$1
-OUTPUT_FILE_PATH=$2
+OUTPUT_FILENAME=$2
 
 # the same as that in fc.py
 HEADER_LINE="device,input_type,layers,nodes,batch_size,input_size,output_size,#params,duration,tflops"
 
-echo $HEADER_LINE >> ${OUTPUT_FILE_PATH}
+# Check the file is exists or not
+if [ -f ${OUTPUT_FILENAME} ]; then
+   rm ${OUTPUT_FILENAME}
+   echo "${OUTPUT_FILENAME} is removed"
+fi
+
+echo $HEADER_LINE >> ${OUTPUT_FILENAME} # >> to write to the end
 
 # sciml21* indicates T4, sciml23* indicates A100
 for filename in "${INPUT_PATH}"/*; do
   echo "Reading data in " ${filename};
-  python3 find_max_flops.py -i ${filename} >> ${OUTPUT_FILE_PATH};
+  python3 find_max_flops.py -i ${filename} >> ${OUTPUT_FILENAME};
 done
