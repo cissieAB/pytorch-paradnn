a26611255b5173c254c7dc1738d95f018b3b31cf
Xinxin (Cissie) Mei <xinxinmei89@gmail.com>
2023-04-22 02:01:19 -0400

    Update heatmap plot

--- scripts/data_processing/find_max_flops.py
+++ scripts/data_processing/find_max_flops.py
@@ -10,8 +10,8 @@ def get_target_line_from_file(df, data_type):
 
 
 def get_dtypes_from_df(df):
-   """Return the data types in the df in a list"""
-   return df['input_type'].unique()
+    """Return the data types in the df in a list"""
+    return df['input_type'].unique()
 
 
 def print_dict(dic):
@@ -32,10 +32,11 @@ def process_args(argv):
 
 def get_title_str(dic):
     device_str = 'T4' if dic["device"] == "Tesla T4" else "A100"
-    return f'{device_str}_{dic["input_type"]}_l_{dic["layers"]}_input_{dic["input_size"]}_output_{dic["output_size"]}'
+    dtype_str = dic["input_type"].replace(" ", "")  # remove space in str
+    return f'{device_str}_{dtype_str}_l_{dic["layers"]}_input_{dic["input_size"]}_output_{dic["output_size"]}'
 
 
-def heatmap_x_node_y_bs(df, fixed_layers, fixed_input, fixed_output, fixed_dtype, title_str=''):
+def heatmap_utilization_x_node_y_bs(df, peak_flops, fixed_layers, fixed_input, fixed_output, fixed_dtype, title_str=''):
     """
     Plot the heatmap performance with given layers, input, output, datatype. Title string is optional.
     """
@@ -45,6 +46,7 @@ def heatmap_x_node_y_bs(df, fixed_layers, fixed_input, fixed_output, fixed_dtype
         & (df['output_size'] == fixed_output)
         & (df['input_type'].str.contains(fixed_dtype))
         ]
+    target_df.loc[:, 'tflops'] *= 100 / peak_flops
 
     if not title_str:
         title_str = f'{fixed_dtype}_l_{fixed_layers}_input_{fixed_input}_output_{fixed_output}'
@@ -53,18 +55,18 @@ def heatmap_x_node_y_bs(df, fixed_layers, fixed_input, fixed_output, fixed_dtype
                                       aggfunc='mean')
     # print(target_tb)
 
-    plt.figure()
+    plt.figure(figsize=(5, 4))
     plt.clf()
     # cmap reference: https://matplotlib.org/stable/tutorials/colors/colormaps.html
     plt.imshow(target_tb, cmap='OrRd')
     plt.colorbar()
 
     # plt.xticks(range(target_tb.shape[1]), np.log2(target_tb.columns).astype(int))
-    plt.xticks(range(target_tb.shape[1]), target_tb.columns.astype(int))
-    plt.xlabel('Hidden layer size')
-    plt.yticks(range(target_tb.shape[0]), target_tb.index.astype(int))
-    plt.ylabel('Batch size')
-    plt.title(f'TFLOPS ({title_str})')
+    plt.xticks(range(target_tb.shape[1]), target_tb.columns.astype(int), size=7)
+    plt.xlabel(r'Hidden layer size ($D_H$)', size=9)
+    plt.yticks(range(target_tb.shape[0]), target_tb.index.astype(int), size=7)
+    plt.ylabel(r'Batch size ($bs$)', size=9)
+    plt.title(f'Utilization rates compared to maximum (%)', size=9)
     plt.savefig(f'heatmap_perf_{title_str}.png')
 
 
@@ -81,10 +83,11 @@ def main(argv):
         target_dict = get_target_line_from_file(df, dt)
         print_dict(target_dict)
         title = get_title_str(target_dict)
-        heatmap_x_node_y_bs(df,
-                           target_dict['layers'], target_dict['input_size'],
-                           target_dict['output_size'], target_dict['input_type'],
-                           title)
+        heatmap_utilization_x_node_y_bs(df,
+                                        target_dict['tflops'],
+                                        target_dict['layers'], target_dict['input_size'],
+                                        target_dict['output_size'], target_dict['input_type'],
+                                        title)
 
 
 if __name__ == "__main__":
